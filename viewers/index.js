c06438d23537be99a3b538830016710044aba908
Nick Hynes <nhynes@mit.edu>
2014-10-08 01:54:15 -0400

    Added silent bad merge exercise

--- viewers/index.js
+++ viewers/index.js
@@ -1,10 +1,12 @@
 'use strict';
 
 var createPushNewFile = require('./createPushNewFile'),
-    collabMerge = require('./collabMerge');
+    collabMerge = require('./collabMerge'),
+    silentBadMerge = require('./silentBadMerge');
 
 // TODO: make this automatic
 module.exports = {
     createPushNewFile: createPushNewFile,
-    collabMerge: collabMerge
+    collabMerge: collabMerge,
+    silentBadMerge: silentBadMerge
 };
