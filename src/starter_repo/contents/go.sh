971b1f4f835960058d6a4173b831632b3acd6e70
Nick Hynes <nhynes@mit.edu>
2014-11-24 21:17:01 -0500

    Remove and reinstate hooks before and after go ops

--- src/starter_repo/contents/go.sh
+++ src/starter_repo/contents/go.sh
@@ -4,10 +4,10 @@ anglerUrl="$baseUrl/hooks"
 
 git config angler.url "$anglerUrl"
 gitroot="$(git rev-parse --show-toplevel)"
-cp "$gitroot"/.gitstream/hooks/* "$gitroot"/.git/hooks/
 
 curl -H "X-GitStream-Repo: $repo" "$baseUrl/go"
 
+rm "$gitroot"/.git/hooks/*
 git reset HEAD :/ > /dev/null 2>&1
 git checkout :/ > /dev/null 2>&1
 git clean -df > /dev/null 2>&1
@@ -16,4 +16,5 @@ git checkout origin/master > /dev/null 2>&1
 git branch -f master > /dev/null 2>&1
 git checkout master > /dev/null 2>&1
 git log --oneline --color --graph --decorate --all
+cp "$gitroot"/.gitstream/hooks/* "$gitroot"/.git/hooks/
 echo "GitStream: Follow the instructions in your browser!"
