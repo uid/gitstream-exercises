973ffc53dca37ef064bd390f047ea1de7b3092a6
Nick Hynes <nhynes@mit.edu>
2015-01-15 09:17:13 -0500

    More updates to go.sh
    Cleaning up old objects
    More accurate patterns for removing origins/refs

--- src/starter_repo/contents/go.sh
+++ src/starter_repo/contents/go.sh
@@ -8,14 +8,17 @@ gitroot="$(git rev-parse --show-toplevel)"
 curl -H "X-GitStream-Repo: $repo" "$baseUrl/go"
 
 rm "$gitroot"/.git/hooks/*
-git checkout -f master > /dev/null 2>&1
-git remote | grep -v origin | xargs -r git remote remove
-git show-ref --tags --heads | grep -v master | cut -d" " -f 2 | xargs -rL 1 git update-ref -d > /dev/null 2>&1
+git remote | grep -v "^origin$" | xargs -rL 1 git remote remove
 git remote prune origin > /dev/null 2>&1
+git show-ref --tags --heads | grep -v "/master$" | cut -d" " -f 2 | xargs -rL 1 git update-ref -d > /dev/null 2>&1
+git checkout -f master > /dev/null 2>&1
 git reset --hard HEAD > /dev/null 2>&1
 git clean -df > /dev/null 2>&1
 git fetch origin > /dev/null 2>&1
 git reset --hard origin/master > /dev/null 2>&1
-git log --oneline --color --graph --decorate --all
+git reflog expire --expire=now --all > /dev/null 2>&1
+git gc --prune=now > /dev/null 2>&1
+"$gitroot"/.gitstream/setup.sh > /dev/null 2>&1
 cp "$gitroot"/.gitstream/hooks/* "$gitroot"/.git/hooks/
+git log --oneline --color --graph --decorate --all
 echo "GitStream: Follow the instructions in your browser!"
