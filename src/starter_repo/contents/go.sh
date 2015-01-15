2b390bcf785b13238a8ec91603cd302f04520db8
Nick Hynes <nhynes@mit.edu>
2015-01-14 21:44:51 -0500

    Go script recovers from new tags, branches, and remotes

--- src/starter_repo/contents/go.sh
+++ src/starter_repo/contents/go.sh
@@ -8,6 +8,10 @@ gitroot="$(git rev-parse --show-toplevel)"
 curl -H "X-GitStream-Repo: $repo" "$baseUrl/go"
 
 rm "$gitroot"/.git/hooks/*
+git checkout -f master > /dev/null 2>&1
+git remote | grep -v origin | xargs -r git remote remove
+git show-ref --tags --heads | grep -v master | cut -d" " -f 2 | xargs -rL 1 git update-ref -d > /dev/null 2>&1
+git remote prune origin > /dev/null 2>&1
 git reset --hard HEAD > /dev/null 2>&1
 git clean -df > /dev/null 2>&1
 git fetch origin > /dev/null 2>&1
