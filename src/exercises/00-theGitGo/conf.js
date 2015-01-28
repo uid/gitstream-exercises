2ede805c959ae5a7aafb50669aec5fa85febfcc5
Nick Hynes <nhynes@mit.edu>
2015-01-28 17:07:11 -0500

    Added clone+setup exercise

---
+++ src/exercises/00-theGitGo/conf.js
@@ -0,0 +1,18 @@
+'use strict'
+
+module.exports = {
+    global: {
+        timeLimit: Infinity
+    },
+
+    machine: {
+        startState: 'done',
+        done: null
+    },
+
+    viewer: {
+        title: 'Getting an existing repository',
+        steps: {},
+        feedback: {}
+    }
+}
