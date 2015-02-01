4a74b4a21dc15700c85dc69719fb94ee1f407f0e
Nick Hynes <nhynes@mit.edu>
2015-02-01 09:40:37 -0500

    Improved go script
    Removed GNU -r flag from xargs
    Silenced entire script cleanly

--- src/starter_repo/contents/go.sh
+++ src/starter_repo/contents/go.sh
@@ -1,3 +1,6 @@
+# silence script
+exec 3>&1 4>&2 1>/dev/null 2>&1
+
 repo="$(git config remote.origin.url)"
 baseUrl=${repo%%/repos*}
 anglerUrl="$baseUrl/hooks"
@@ -6,32 +9,32 @@ git config angler.url "$anglerUrl"
 gitroot="$(git rev-parse --show-toplevel)"
 
 curl -sfH "X-GitStream-Repo: $repo" "$baseUrl/go"
-if [ $? != 0 ]; then echo 'GitStream: Unable to connect to server.'; exit 1; fi
+if [ $? != 0 ]; then >&4 echo 'GitStream: Unable to connect to server.'; exit 1; fi
 
 # remove hooks to prevent the following from triggering events
 rm "$gitroot"/.git/hooks/*
 
 # remove added remotes and refs
-git remote | grep -v "^origin$" | xargs -rL 1 git remote remove
-git remote prune origin > /dev/null 2>&1
-git show-ref --tags --heads | grep -v "/master$" | cut -d" " -f 2 | xargs -rL 1 git update-ref -d > /dev/null 2>&1
+git remote | grep -v "^origin$" | xargs -L 1 git remote remove
+git remote prune origin
+git show-ref --tags --heads | grep -v "/master$" | cut -d" " -f 2 | xargs -L 1 git update-ref -d
 
 # move back to master, clean up the sandbox, and reset to origin state
-git checkout -f master > /dev/null 2>&1
-git reset --hard HEAD > /dev/null 2>&1
-git clean -df > /dev/null 2>&1
-git fetch origin > /dev/null 2>&1
-git reset --hard origin/master > /dev/null 2>&1
+git checkout -f master
+git reset --hard HEAD
+git clean -df
+git fetch origin
+git reset --hard origin/master
 
 # remove unnecessary blobs
-git reflog expire --expire=now --all > /dev/null 2>&1
-git gc --prune=now > /dev/null 2>&1
+git reflog expire --expire=now --all
+git gc --prune=now
 
 # run the setup script, if one is provided
-"$gitroot"/.gitstream/setup.sh > /dev/null 2>&1
+"$gitroot"/.gitstream/setup.sh
 
 # reinstate the hooks
 cp "$gitroot"/.gitstream/hooks/* "$gitroot"/.git/hooks/
 
 # let the user know that everything went well
-echo "GitStream: Follow the instructions in your browser!"
+>&3 echo "GitStream: Follow the instructions in your browser!"
