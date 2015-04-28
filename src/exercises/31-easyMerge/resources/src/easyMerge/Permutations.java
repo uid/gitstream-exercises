29d04427b5deaf65544eecdac10c75715212c773
George Du <gdu@mit.edu>
2015-04-28 02:25:53 -0400

    new exercise: conflict-free merge

---
+++ src/exercises/31-easyMerge/resources/src/easyMerge/Permutations.java
@@ -0,0 +1,26 @@
+package easyMerge;
+
+public class Permutations {
+    public static void main(String[] args) {
+        printPermutations("abcd");
+    }
+
+    /**
+     * Prints all permutations of the input string to System.out.
+     * @param s a sequence of characters with no repeats
+     */
+    public static void printPermutations(String s) {
+        // Implement this!!
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Private helper method for printPermutations.
+     * Example: printPermutations("blah","ab") will print "abblah" and "bablah"
+     * @param tail A string that will be printed after every permutation of s
+     * @param s The string for which all permutations will be printed
+     */
+    private static void printPermutations(String tail, String s) {
+        throw new UnsupportedOperationException();
+    }
+}
