384d5a1c1fe9a8c3a7cf42306cf7a4c6ae831eee
Max Goldman <maxg@mit.edu>
2015-04-28 21:52:30 -0400

    Improve merge conflict instructions

--- src/exercises/22-mcJava/conf.js
+++ src/exercises/22-mcJava/conf.js
@@ -67,9 +67,9 @@ module.exports = {
 
         steps: {
             editFile: 'Import the project into Eclipse, and implement the argmax method. Make sure the test passes, and commit your work.',
-            pushCommit: 'Push your commit',
-            pullRepo: 'Your collaborator has pushed a new commit, so your repo is out of date! <a href="http://www.git-scm.com/docs/git-pull" target="_blank">Pull</a> the repo to get the latest changes.',
-            mergeFile: 'There was a merge conflict! Edit the file and merge the changes. When the tests pass, add, commit, and push!'
+            pushCommit: 'Push your commit.',
+            pullRepo: 'Your collaborator has pushed a new commit, so your repo is out of date! Pull the repo to get the latest changes.',
+            mergeFile: 'There was a merge conflict! Edit the file and merge the changes (remember to refresh the Eclipse project). When the tests pass, add, commit, and push!'
         },
 
         feedback: {
