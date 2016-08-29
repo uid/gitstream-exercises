f668492408f39cda19c298f2251e50043476a809
Nick Hynes <nhynes@mit.edu>
2016-08-29 15:56:49 -0400

    Add some history to theGitGo

--- src/exercises/00-theGitGo/conf.js
+++ src/exercises/00-theGitGo/conf.js
@@ -14,5 +14,19 @@ module.exports = {
         title: 'Getting an existing repository',
         steps: {},
         feedback: {}
+    },
+
+    repo: {
+        commits: [
+            { msg: 'Initial commit', files: [] },
+            {
+                msg: 'Add hello.txt',
+                files: [{ src: 'hello.txt', template: { greeting: 'Hello, world!' } }]
+            },
+            {
+                msg: 'Update hello.txt',
+                files: [{ src: 'hello.txt', template: { greeting: 'Hello, Git!' } }]
+            }
+        ]
     }
 }
