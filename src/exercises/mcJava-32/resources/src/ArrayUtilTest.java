ed8c3fa15f9c2cc6db9d237880ccb5fd1f912707
Max Goldman <maxg@mit.edu>
2019-04-18 15:00:41 -0400

    Update Java exercise classpaths and JUnit

--- src/exercises/mcJava-32/resources/src/ArrayUtilTest.java
+++ src/exercises/mcJava-32/resources/src/ArrayUtilTest.java
@@ -1,6 +1,6 @@
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class ArrayUtilTest {
     @Test
